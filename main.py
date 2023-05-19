d45e20cbfd574989f1b38617663306edb1224e25
minh.pham2000 <minh.pham2000@linecorp.com>
2023-05-19 23:22:47 +0700

    Add resnet50

--- main.py
+++ main.py
@@ -1,23 +1,66 @@
 import argparse
+import os
 
-import torchvision.transforms as transforms
-from datasets.imagenet import ImageNet1k
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
+from torchvision.transforms import (Compose, Normalize, RandomCrop,
+                                    RandomHorizontalFlip, ToTensor)
+from torchvision.transforms.v2 import RandomResize
 
+from datasets.imagenet import ImageNet1k
+from models import ResNet50
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--train_dir", type=str, required=True)
-# parser.add_argument("--val_dir", type=str, required=True)
+parser.add_argument("--val_dir", type=str, required=True)
+parser.add_argument("--wandb_api_key", type=str, required=True)
 args = vars(parser.parse_args())
 
-dataset = ImageNet1k(label_files=args["train_dir"])
-print(len(dataset))
+os.environ["WANDB_API_KEY"] = args.get("wandb_api_key")
 
-dataset = ImageNet1k(label_files=args["train_dir"], transform=transforms.Compose([
-    transforms.ToTensor(),
-    transforms.RandomCrop(size=224),
-]))
-loader = DataLoader(dataset=dataset, batch_size=32)
-for batch in loader:
-    print(batch)
-    break
+seed_everything(42)
+
+train_transform = Compose([
+    RandomResize(min_size=256, max_size=480),
+    RandomHorizontalFlip(),
+    RandomCrop(224),
+    ToTensor(),
+    Normalize([0.49139968, 0.48215841, 0.44653091],
+              [1., 1., 1.]),  
+])
+
+test_transform = Compose([
+    ToTensor(),
+    Normalize([0.49139968, 0.48215841, 0.44653091],
+              [1., 1., 1.])
+])
+
+train_dataset = ImageNet1k(label_files=args["train_dir"], transform=train_transform)
+val_dataset = ImageNet1k(label_files=args["val_dir"], transform=test_transform)
+
+train_loader = DataLoader(train_dataset,
+                          batch_size=256,
+                          shuffle=True,
+                          num_workers=4)
+val_loader = DataLoader(val_dataset,
+                        batch_size=256,
+                        shuffle=False,
+                        num_workers=4)
+
+model = ResNet50(num_classes=1000)
+
+pl_trainer = Trainer(accelerator="gpu",
+                     max_epochs=100,
+                     enable_progress_bar=False,
+                     callbacks=[
+                         ModelCheckpoint("chkpt",
+                                         monitor=ResNet50.VAL_TOP1_ACC_KEY,
+                                         mode="max"),
+                     ],
+                     logger=WandbLogger(project="ImageNet1k",
+                                        name="ImageNet1k_ResNet50"))
+pl_trainer.fit(model=model,
+               train_dataloaders=train_loader,
+               val_dataloaders=val_loader)
---
+++ models/resnet50.py
@@ -0,0 +1,89 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchmetrics import Accuracy
+from torchvision.models import resnet50
+
+
+class ResNet50(pl.LightningModule):
+    TRAIN_LOSS_KEY = "train_loss"
+    TRAIN_TOP1_ACC_KEY = "train_top1_acc"
+    TRAIN_TOP5_ACC_KEY = "train_top5_acc"
+
+    VAL_LOSS_KEY = "val_loss"
+    VAL_TOP1_ACC_KEY = "val_top1_acc"
+    VAL_TOP5_ACC_KEY = "val_top5_acc"
+
+    def __init__(self,
+                 num_classes: int) -> None:
+        super().__init__()
+        self.num_classes = num_classes
+
+        self.resnet_model = resnet50()
+        self.resnet_model.fc = nn.Linear(2048, self.num_classes)
+
+        self.train_top1_acc = Accuracy(num_classes=self.num_classes,
+                                       top_k=1,
+                                       task="multiclass")
+        self.train_top5_acc = Accuracy(num_classes=self.num_classes,
+                                       top_k=5,
+                                       task="multiclass")
+        self.val_top1_acc = Accuracy(num_classes=self.num_classes,
+                                     top_k=1,
+                                     task="multiclass")
+        self.val_top5_acc = Accuracy(num_classes=self.num_classes,
+                                     top_k=5,
+                                     task="multiclass")
+
+        self.train_loss = None
+        self.val_loss = None
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.resnet_model(x)
+
+    def training_step(self, batch, batch_idx) -> torch.Tensor:
+        imgs, labels = batch
+        preds = self.forward(imgs)
+
+        loss = F.cross_entropy(preds, labels)
+        self.train_loss = loss.item()
+
+        self.train_top1_acc(preds, labels)
+        self.train_top5_acc(preds, labels)
+
+        self.log(self.TRAIN_LOSS_KEY, loss)
+        self.log(self.TRAIN_TOP1_ACC_KEY, self.train_top1_acc)
+        self.log(self.TRAIN_TOP5_ACC_KEY, self.train_top5_acc)
+
+        return loss
+
+    def validation_step(self, val_batch, val_idx):
+        imgs, labels = val_batch
+        preds = self.forward(imgs)
+
+        loss = F.cross_entropy(preds, labels)
+        self.val_loss = loss.item()
+
+        self.val_top1_acc(preds, labels)
+        self.val_top5_acc(preds, labels)
+
+        self.log(self.VAL_LOSS_KEY, loss)
+        self.log(self.VAL_TOP1_ACC_KEY, self.val_top1_acc)
+        self.log(self.VAL_TOP5_ACC_KEY, self.val_top5_acc)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), 
+                                    lr=0.1,
+                                    weight_decay=0.0001, 
+                                    momentum=0.9)
+        milestone_lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, 
+                                                                      milestones=[int(6e4)], 
+                                                                      gamma=0.1)
+        plateau_lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                                          factor=0.1)
+        return [optimizer], [milestone_lr_scheduler, plateau_lr_scheduler]
+
+    def on_train_epoch_end(self) -> None:
+        print(f"Epoch {self.current_epoch}, " + f"Train loss: {self.train_loss}, " +
+              f"Train top 1 acc: {self.train_top1_acc.compute()}, " + f"Train top 5 acc: {self.train_top5_acc.compute()}")
