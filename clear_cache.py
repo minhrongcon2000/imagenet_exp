52bc654e771dff6558b05de5fa62d39916df836c
minh.pham2000 <minh.pham2000@linecorp.com>
2023-05-27 16:35:00 +0700

    Add wandb api key to clear_cache

--- clear_cache.py
+++ clear_cache.py
@@ -1,12 +1,16 @@
 import argparse
+import os
 
 import wandb
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--wandb_api_key", type=str, required=True)
 parser.add_argument("--project_name", type=str, required=True)
 parser.add_argument("--max_version", type=int, required=True)
 parser.add_argument("--artifact_prefix", type=str, required=True)
 args = vars(parser.parse_args())
+
+os.environ["WANDB_API_KEY"] = args["wandb_api_key"]
 
 api = wandb.Api(overrides=dict(project=args["project_name"]))
 
