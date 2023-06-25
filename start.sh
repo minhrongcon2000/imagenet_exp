d48e88159aee77826619fcd97cac05e5515a8941
minh.pham2000 <minh.pham2000@linecorp.com>
2023-06-25 12:12:10 +0700

    Add neccessary log

--- start.sh
+++ start.sh
@@ -8,5 +8,7 @@ do
         n) num_devices=${OPTARG};;
     esac
 done
+echo Increase ulimit...
 ulimit -n 4096
+echo Execute training script...
 python main.py --train_dir $train_dir --val_dir $val_dir --wandb_api_key $wandb_api_key --device gpu --num_devices $num_devices
