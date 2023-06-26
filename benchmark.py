e42e5dc0f31cfb665c8ebad8ebaf965238f39be1
minh.pham2000 <minh.pham2000@linecorp.com>
2023-06-27 00:28:28 +0700

    Add benchmark code

---
+++ benchmark.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+
+from torch.utils.data import DataLoader
+from torchvision.transforms import (
+    CenterCrop,
+    Compose,
+    Normalize,
+    RandomHorizontalFlip,
+    RandomResizedCrop,
+    Resize,
+    ToTensor,
+)
+from torchvision.datasets import ImageFolder
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--train_dir", type=str, required=True)
+args = vars(parser.parse_args())
+
+os.environ["WANDB_API_KEY"] = args.get("wandb_api_key")
+
+
+train_transform = Compose(
+    [
+        ToTensor(),
+        RandomResizedCrop(224),
+        RandomHorizontalFlip(),
+        Normalize(
+            mean=[0.49139968, 0.48215841, 0.44653091],
+            std=[0.24703223, 0.24348513, 0.26158784],
+        ),
+    ]
+)
+
+train_dataset = ImageFolder(
+    root=args.get("train_dir"),
+    transform=train_transform,
+)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=args.get("batch_size"),
+    shuffle=True,
+    num_workers=args.get("num_workers"),
+)
+
+for i, (batchX, batchY) in enumerate(train_loader):
+    print(i, batchX.shape, batchY.shape)
